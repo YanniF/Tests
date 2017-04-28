1d0adfc00877c7e7a9cadcaee8d437a271136918
Yanni Fraga <Yanni Fraga>
2017-04-28 15:06:38 -0700

    Periscope effect

--- Parallax/js/functions.js
+++ Parallax/js/functions.js
@@ -13,4 +13,16 @@ $(window).scroll(function() {
 	$('.fore-bird').css({
 		'transform' : 'translate(0, -' + wScroll/40 + '%)' 
 	});
+
+	//when the top of clothes-pics hits the top of the window - 20% within the viewport of the browser
+	if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+		
+		//for each figure execute this function
+		$('.clothes-pics figure').each(function(i) {
+
+			setTimeout(function() {
+				$('.clothes-pics figure').eq(i).addClass('is-showing');
+			}, 150 * (i + 1));
+		});
+	}
 });
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Repository for tests (usually front-end)
 - Animate text over images
 - CSS Full Screen Slider
 - Fixed Background Scrolling
+- Parallax
 - Video As A Background For Section Of Website
 - Video as Background for a Section of a Website
 - Video as Background and Playing with Filters
