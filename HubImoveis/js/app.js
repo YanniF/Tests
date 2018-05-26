e7d23eab02d78540251f80533d22444973d970d0
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-05-26 13:05:33 -0300

    Getting the JSON data

--- HubImoveis/js/app.js
+++ HubImoveis/js/app.js
@@ -1,19 +1,42 @@
-document.addEventListener('DOMContentLoaded', carregarImoveis);
+$(document).ready(function(){
+  
+  $.getJSON("js/release_response.json", function(info) {
 
-function carregarImoveis() {
+    var texto = "";
+    var imoveis = info.data.content;
 
-  let imoveis;
-
-  fetch('js/release_response.json')
-  .then(r => r.json())
-  .then(info => {
-    imoveis = info.data.content;
-    
-    imoveis.forEach(imovel => {
-      console.log(`${imovel.title} \n${imovel.city} - ${imovel.state} \n${imovel.neighborhood}`);
+    $.each(imoveis, function(i) {
+      texto = `
+        <img src="${this.thumbUrl}" id="img-${i}" class="img-fluid" alt="${this.title}">
+        <figcaption>
+          <h3>${this.title}</h3>
+          <p>${this.city} - ${this.state}</p>
+          <p>${this.neighborhood}</p>
+          <img src="${this.logoUrl}" class="logo" alt="${this.companyName}">
+        </figcaption>
+      `;
+      $('#figure-' + i).append(texto);
     });    
-  })
-  .catch(err => {
-    console.log('Erro: ' + err);
+  });
+
+  setTimeout(initSlick, 100); //aprender a esperar a resposta da requisição
+  
+});
+
+function initSlick() {
+  $('.slider-imagens').on('init', function(slick) {
+    $("#img-0").addClass('slick-active');
+  });
+
+  $('.slider-imagens').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+
+  $('.slider-imagens').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    $("#img-" + currentSlide).removeClass('slick-active');
+    $("#img-" + nextSlide).addClass('slick-active'); 
+    $('.controls span').text((nextSlide + 1) + "/6");
   });
 }
